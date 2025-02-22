5ec45e75c5f572be8bc5d52f1e8a2aec0bfa2cdd
Alina <alinakubikalex@gmail.com>
2025-02-22 15:35:21 +0100

    add nav script

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+const navButton = document.querySelector('.header__navigation-button');
+const navSection = document.querySelector('.navigation');
+
+function onClicked() {
+  if (navButton.classList.contains('open-navigation')) {
+    navSection.classList.remove('visually-hidden');
+    navButton.classList.add('close-navigation');
+    navButton.classList.remove('open-navigation');
+  } else {
+    navSection.classList.add('visually-hidden');
+    navButton.classList.remove('close-navigation');
+    navButton.classList.add('open-navigation');
+  }
+}
+navButton.addEventListener('click', onClicked);
