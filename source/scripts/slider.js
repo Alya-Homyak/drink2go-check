ccde848e8587a56612c18cbda9149be4b558de98
Alina <alinakubikalex@gmail.com>
2025-02-23 22:13:02 +0100

    fix slider pagination

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,31 +1,34 @@
 const slides = document.querySelectorAll('.slide');
+const paginationButtons = document.querySelectorAll('.hero__pagination-button');
 let isLast = false;
 let isFirst = true;
-
-function changeSlide(activeSlide) {
-  const active = document.querySelector('.slide--active');
-  active.classList.remove('slide--active');
-  slides[activeSlide].classList.add('slide--active');
-}
-
 let current = 0;
+
 function nextSlide() {
   current += 1;
-  changeSlide(current);
-  checkBoundaries(current);
+  changeSlide();
+  checkBoundaries();
   changeButtonState();
-  changePagState(current);
+  changePaginationState();
 }
 
 function prevSlide() {
   current -= 1;
-  changeSlide(current);
-  checkBoundaries(current);
+  changeSlide();
+  checkBoundaries();
   changeButtonState();
-  changePagState(current);
+  changePaginationState();
 }
 
-function checkBoundaries(current) {
+function goTo(slideIndex) {
+  current = slideIndex;
+  changeSlide();
+  checkBoundaries();
+  changeButtonState();
+  changePaginationState();
+}
+
+function checkBoundaries() {
   isFirst = current === 0;
   isLast = current === slides.length - 1;
 }
@@ -44,27 +47,32 @@ function changeButtonState() {
   }
 }
 
+function changeSlide() {
+  const active = document.querySelector('.slide--active');
+  active.classList.remove('slide--active');
+  slides[current].classList.add('slide--active');
+}
+
+function changePaginationState() {
+  document.querySelector('.hero__pagination-button--active').classList.remove('hero__pagination-button--active');
+  paginationButtons[current].classList.add('hero__pagination-button--active');
+}
+
+//-------------------------
 const prevButton = document.querySelector('.hero__slider-button--prev');
 prevButton.addEventListener('click', prevSlide);
 
 const nextButton = document.querySelector('.hero__slider-button--next');
 nextButton.addEventListener('click', nextSlide);
 
-//pagination
-const pagButtons = document.querySelectorAll('.hero__pagination-button');
+function onPaginationClick(event) {
+  if (!event.target.classList.contains('hero__pagination-button')) {
+    return;
+  }
 
-function changeSlideByPag(clicked) {
-  current = (clicked.target.dataset.number) - 1;
-  changeSlide(current);
-  checkBoundaries(current);
-  changeButtonState();
-  changePagState(current)
+  const slideIndex = parseInt(event.target.dataset.slideIndex, 10);
+  goTo(slideIndex);
 }
 
-function changePagState(current) {
-  document.querySelector('.hero__pagination-button--active').classList.remove('hero__pagination-button--active');
-  pagButtons[current].classList.add('hero__pagination-button--active');
-}
-
-const pagContainer = document.querySelector('.hero__pagination');
-pagContainer.addEventListener('click', changeSlideByPag)
+const paginationContainer = document.querySelector('.hero__pagination');
+paginationContainer.addEventListener('click', onPaginationClick)
